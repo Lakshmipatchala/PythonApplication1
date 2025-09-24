f9bce9d0d9bc6850ab57c903ee1702d1e9ef7a31
nlp1903-rgb <nlp1903@googlemail.com>
2025-09-24 04:53:32 -0500

    Update string_1.py with latest modifications

--- PythonApplication1/string_1.py
+++ PythonApplication1/string_1.py
@@ -1,6 +1,7 @@
 
 first_name ='laks'
 last_na='pat'
-out='Hello , {1} {0}'.format(first_name,last_na)
+#code has been changed
+out='Hello , {1} {0}'.format(first_name,last_na),"this is changed code"
 
 print(out)
